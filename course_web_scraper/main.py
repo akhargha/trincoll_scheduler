1d7ee5281698d28e13290c6d4c0de0e4fa4c9872
akhargha <kharghariaanupam07@gmail.com>
2023-06-16 02:14:51 -0400

    File Organization

--- course_web_scraper/main.py
+++ course_web_scraper/main.py
@@ -1,1 +1,10 @@
+from bs4 import BeautifulSoup
 
+import requests
+
+html_text = requests.get('https://internet3.trincoll.edu/ptools/courselisting.aspx').text
+soup = BeautifulSoup(html_text,'lxml')
+items = soup.select('option[value]')
+values = [item.get('value') for item in items]
+textValues = [item.text for item in items]
+print(textValues)
