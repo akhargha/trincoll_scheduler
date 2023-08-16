a88f8de0dd6f96b1078d9b26f9b66495edccda8e
Anupam Khargharia <kharghariaanupam07@gmail.com>
2023-08-16 03:57:51 -0400

    Create courses table for database schema

--- database_sql/schedule-ddl.sql
+++ database_sql/schedule-ddl.sql
@@ -7,3 +7,13 @@ CREATE TABLE Users (
     first_name VARCHAR(255),
     last_name VARCHAR(255),
 );
+
+-- Courses Table
+CREATE TABLE Courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_code VARCHAR(255) NOT NULL UNIQUE,
+    course_name VARCHAR(255) NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    days_of_week VARCHAR(255) NOT NULL
+);
