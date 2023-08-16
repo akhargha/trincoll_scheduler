8da3ccd1b73c8394d2f59f4e9f47104cc299f0eb
Anupam Khargharia <kharghariaanupam07@gmail.com>
2023-08-16 17:33:43 -0400

    Add retrieve course of user query

--- database_sql/queries.sql
+++ database_sql/queries.sql
@@ -11,3 +11,10 @@ SELECT 'Search for Course using course code:' as Query;
 SELECT * 
 FROM Courses 
 WHERE course_code = '101';
+
+-- Retreive all courses by a specific user (Anupam in this case)
+SELECT 'Retreive all courses by a specific user:' as Query;
+SELECT C.course_name 
+FROM UserCourses UC
+JOIN Courses C ON UC.course_id = C.course_id
+WHERE UC.user_id = 1;
