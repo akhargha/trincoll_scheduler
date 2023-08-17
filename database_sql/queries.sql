2bc03edbaa33bffe91ffc6f3ad5c0c531d4b3ab7
Anupam Khargharia <kharghariaanupam07@gmail.com>
2023-08-17 03:24:48 -0400

    Add friend social feature queries

--- database_sql/queries.sql
+++ database_sql/queries.sql
@@ -18,3 +18,25 @@ SELECT C.course_name
 FROM UserCourses UC
 JOIN Courses C ON UC.course_id = C.course_id
 WHERE UC.user_id = 1;
+
+-- Retreive all friends of a specific user (Lily in this case)
+SELECT 'Retreive all friends of a specific user:' as Query;
+SELECT U.username AS friend_name 
+FROM Friends F
+JOIN Users U ON (F.user_id = U.user_id OR F.friend_id = U.user_id)
+WHERE (F.user_id = 2 OR F.friend_id = 2) AND U.user_id != 2 AND F.status = 'Accepted';
+
+-- Retreive all pending friend requests of a specific user (Lily in this case)
+SELECT 'Retreive all pending friend requests of a specific user' as Query;
+SELECT U.username AS friend_request_from, F.date_sent 
+FROM Friends F
+JOIN Users U ON F.user_id = U.user_id
+WHERE F.friend_id = 2 AND F.status = 'Pending';
+
+-- Retreive all courses added by friends for a specific user (in this case, for Anupam)
+SELECT 'Retreive all courses added by friends for a specific user' as Query;
+SELECT C.course_name, U.username AS added_by, A.timestamp AS date_added
+FROM AuditTrail A
+JOIN Courses C ON A.course_id = C.course_id
+JOIN Users U ON A.friend_id = U.user_id
+WHERE A.user_id = 1;
