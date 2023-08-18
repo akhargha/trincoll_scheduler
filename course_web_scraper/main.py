4c31fbaa5df11b9cfd4b594dab5cf684d2d6cac7
Anupam Khargharia <kharghariaanupam07@gmail.com>
2023-08-17 23:55:51 -0400

    Fix redirect bug and export to .csv

--- course_web_scraper/main.py
+++ course_web_scraper/main.py
@@ -1,29 +1,73 @@
-from bs4 import BeautifulSoup
+import time
+
+import pandas as pd
 import requests
+from bs4 import BeautifulSoup
+from tabulate import tabulate
 
 url = "https://internet3.trincoll.edu/ptools/courselisting.aspx"
 
-with requests.session() as s:
-    soup = BeautifulSoup(s.get(url).content, "html.parser")
-    courses = [v["value"] for v in soup.select("#ddlList [value]")]
 
-    for c in courses:
-        data = {}
-        for inp in soup.select("input[value][name]"):
-            data[inp["name"]] = inp["value"]
+def wait_a_bit(wait_for: int = 5) -> None:
+    time.sleep(wait_for)
 
-        data["ddlList"] = c
-        data["ddlLevelList"] = "0"
-        data["ddlTermList"] = "1241"  # <-- Fall 2023
-        data["ddlSession"] = "0"
-        data["__EVENTTARGET"] = "ddlList"
-        data["__EVENTARGUMENT"] = ""
-        data["__LASTFOCUS"] = ""
 
-        soup = BeautifulSoup(s.post(url, data=data).content, "html.parser")
+with requests.session() as session:
+    course_selection = (
+        BeautifulSoup(session.get(url).text, "html.parser")
+        .select("#ddlList [value]")
+    )
+    all_courses = [[c.get("value"), c.getText()] for c in course_selection]
 
-        print(c)
-        print("-" * 80)
-        for id_, time, title, c_id in zip(soup.select(".TITLE_id"), soup.select(".TITLE_times"), soup.select(".TITLE_title"), soup.find_all('td', nowrap="true")):
-            print(f"{id_.text:<10} {title.text} {time.text} {c_id.text}")
-        print()
+    for course in all_courses:
+        course_id, course_name = course
+        print(f"Getting {course_name}...")
+
+        v_state = (
+            BeautifulSoup(session.get(url).text, "html.parser")
+            .find("input", {"name": "__VIEWSTATE"})["value"]
+        )
+        event_validation = (
+            BeautifulSoup(session.get(url).text, "html.parser")
+            .find("input", {"name": "__EVENTVALIDATION"})["value"]
+        )
+
+        payload = {
+            "__EVENTVALIDATION": event_validation,
+            "__VIEWSTATE": v_state,
+            "__VIEWSTATEGENERATOR": "3786FA90",
+            "rblLstType": 1,
+            "ddlList": course_id,
+            "ddlLevelList": 0,
+            "ddlTermList": 1241,
+            "ddlSession": 0,
+            "btnSubmit": "Submit",
+        }
+
+        response = session.post(url, data=payload)
+        courses = (
+            BeautifulSoup(response.text, "html.parser")
+            .select_one('table[class="TITLE_tbl"]')
+        )
+
+        table_rows = zip(
+            courses.select(".TITLE_id"),
+            courses.select(".TITLE_times"),
+            courses.select(".TITLE_title"),
+            courses.find_all('td', nowrap="true"),
+        )
+
+        my_table = [
+            [
+                id_.getText(),
+                time.getText(),
+                title.getText(),
+                c_id.getText(),
+            ] for id_, time, title, c_id in table_rows
+        ]
+
+        df = pd.DataFrame(my_table, columns=["ID", "Time", "Title", "Course ID"])
+        df.to_csv(f"{course_id}_{course_name}.csv", index=False)
+        print(tabulate(df, headers="keys", tablefmt="psql", showindex=False))
+
+        wait_a_bit(wait_for=3)
